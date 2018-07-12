450bf93703246243b530366c2dee7b0f84e52c38
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-12 02:15:05 +0000

    automatically backed up by learn

--- demo.rb
+++ demo.rb
@@ -1,9 +1,9 @@
-def using_while
-	levitation_force = 0
-	  while levitation_force<10 
-	  puts "wingardium leviosa"
-	  levitation_force += 1
-	end
-	puts "y0r a lizard now harry"
-end
-using_while
+# def using_while
+# 	levitation_force = 0
+# 	  while levitation_force<10 
+# 	  puts "wingardium leviosa"
+# 	  levitation_force += 1
+# 	end
+# 	puts "y0r a lizard now harry"
+# end
+# using_while
