88bb9a4e08cb9067c2b1c8882bc7118d1139913d
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-12 02:15:18 +0000

    automatically backed up by learn

--- demo.rb
+++ demo.rb
@@ -9,3 +9,12 @@
 # using_while
 
 
+def using_until
+        levitation_force = 6
+        until levitation_force == 10
+        puts "wingardium leviosa"
+        levitation_force += 1
+      end
+puts "y0r a lizard harry, your levitation_force is at #{levitation_force}!"
+end
+using_until
